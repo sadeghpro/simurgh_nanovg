49db4e5cd61f428cddb8a240fcf79803066138db
sadeghpro <sadeghmohamadnia@yahoo.com>
2020-05-01 22:47:04 +0430

    Add ios

---
+++ desktop/src/main/c/jni/JniNanoVGGLES3.c
@@ -0,0 +1,18 @@
+#include <jni.h>
+
+#define NANOVG_GLES3_IMPLEMENTATION
+
+#include <OpenGLES/ES3/glext.h>
+#include "../nanovg/src/nanovg.h"
+#include "../nanovg/src/nanovg_gl.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT jlong JNICALL Java_dev_simurgh_core_jni_nanovg_NanoVGGLES3_nvgCreate(JNIEnv *env, jclass clazz, jint flags) {
+    return (jlong)nvgCreateGLES3(flags);
+}
+
+#ifdef __cplusplus
+}
+#endif
